44d388ce65206df3e759cde9e8d0a8dac7e25083
d.karpenko <d.karpenko@mkomov.com>
2025-08-07 06:28:07 +0300

    init-project

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import pugPlugin from 'vite-plugin-pug'
+
+const locals = { name: 'components' }
+
+export default defineConfig({
+  plugins: [pugPlugin(locals)],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name].css',
+        chunkFileNames: 'assets/[name].js'
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  }
+})
